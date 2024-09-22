1f5b97175861898d4612ed6eb0a148149c76b9f9
depvvrld <sashashakhanov@gmail.com>
2024-09-22 21:21:28 +0300

    creation printVector

--- main.cpp
+++ main.cpp
@@ -16,6 +16,12 @@ void sortByInsert(vector <int> &arr) {
 }
 
 
+void printVector(vector <int>& vec) {
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << '\t';
+	}
+}
+
 int main() {
 
 	return 0;
