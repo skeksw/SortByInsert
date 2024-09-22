43f1a786ea1533a5b11d85bae17ac44edd6cd748
skeksw <ilya.21082018@gmail.com>
2024-09-22 22:47:57 +0300

    addition menu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#include "menu.h"
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 vector <int> sortByInsert(vector <int> &vec) {
@@ -19,18 +21,57 @@ vector <int> sortByInsert(vector <int> &vec) {
 
 void printVector(vector <int>& vec) {
 	for (int i = 0; i < vec.size(); i++) {
-		cout << vec[i] << '\t';
+        cout << vec[i] << '\t';
 	}
+    cout << endl;
 }
 
 vector <int> randomVectorFill(vector<int> &vec) {
-    for (int i = 0; i < vec.size(); i++) {
-        vec[i] = rand() % 100 - 50;
+    for (int i = 0; i < rand() % 15; i++) {
+        vec.push_back(rand() % 100 - 50);
+        
+    }
+    return vec;
+}
+vector <int> manualVectorFill(vector<int>& vec) {
+    cout << "Enter the number of size array:" << endl;
+    int sizeOfArray = checkData();
+    int number = 0;
+    cout << "Enter the array:" << endl;
+    for (int i = 0; i < sizeOfArray; i++) {
+        cout << i << ": " << endl;
+        cin >> number;
+        vec.push_back(number);
     }
     return vec;
 }
 
 int main() {
-
+    menuEnum choice = menu();
+    vector <int> vec;
+    while (choice != menuEnum::exitEnum) {
+        switch (choice)
+        {
+        case randomFill:
+            randomVectorFill(vec);
+            cout << "Not sorted array:" << endl;
+            printVector(vec);
+            cout << "Sorted array (insert):" << endl;
+            sortByInsert(vec);
+            printVector(vec);
+            vec.clear();
+            break;
+        case manual:
+            manualVectorFill(vec);  
+            cout << "Sorted array (insert):" << endl;
+            sortByInsert(vec);
+            printVector(vec);
+            vec.clear();
+            break;
+        default:
+            cout << "Press from 1 to 3" << endl;
+        }
+        choice = menu();
+    }
 	return 0;
 }
---
+++ menu.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "checkData.h"
+
+enum menuEnum menu();
+
+enum menuEnum {
+	randomFill = 1,
+	manual,
+	exitEnum
+};
