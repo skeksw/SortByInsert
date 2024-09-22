c0695cd7e55c414370dba1e55c05ec7a153d2653
skeksw <ilya.21082018@gmail.com>
2024-09-22 21:15:39 +0300

    create sortByInsert

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+void sortByInsert(vector <int> &arr) {
+    for (int i = 1; i < arr.size(); i++) {  
+        for (int k = i; k > 0; k--) {
+            if ((arr[k]) < (arr[k - 1])) {
+                swap(arr[k], arr[k - 1]);
+            }
+            else {
+                break;
+            }
+        }  
+    }
+}
+
 
 int main() {
 
