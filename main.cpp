e255392d359ec0438744004efd464682dfccaa95
depvvrld <sashashakhanov@gmail.com>
2024-09-22 21:21:28 +0300

    creation printVector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void printVector(vector <int>& vec) {
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << '\t';
+	}
+}
 
 int main() {
 
